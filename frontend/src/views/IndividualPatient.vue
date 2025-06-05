47bf9ce2704268d60a256d795e9cf49375b76031
biancab20 <705366@student.inholland.nl>
2025-06-05 19:34:46 +0200

    individual patient view without ecg

--- frontend/src/views/IndividualPatient.vue
+++ frontend/src/views/IndividualPatient.vue
@@ -5,29 +5,29 @@
         <div class="text-h6">Patient Information</div>
       </q-card-section>
 
-      <q-card-section v-if="patient">
+      <q-card-section>
         <q-list>
           <q-item>
-            <q-item-section><strong>Name:</strong> {{ patient.name }}</q-item-section>
+            <q-item-section><strong>Name:</strong> {{ name }}</q-item-section>
           </q-item>
           <q-item>
-            <q-item-section><strong>Birth Date:</strong> {{ patient.birth_date }}</q-item-section>
+            <q-item-section><strong>Birth Date:</strong> {{ birth_date }}</q-item-section>
           </q-item>
           <q-item>
-            <q-item-section><strong>Gender:</strong> {{ patient.gender }}</q-item-section>
+            <q-item-section><strong>Gender:</strong> {{ gender }}</q-item-section>
           </q-item>
           <q-item>
-            <q-item-section><strong>Contact Info:</strong> {{ patient.contact_info }}</q-item-section>
+            <q-item-section><strong>Contact Info:</strong> {{ contact_info }}</q-item-section>
           </q-item>
           <q-item>
-            <q-item-section><strong>Created At:</strong> {{ patient.created_at }}</q-item-section>
+            <q-item-section><strong>Created At:</strong> {{ created_at }}</q-item-section>
           </q-item>
         </q-list>
       </q-card-section>
 
-      <q-card-section v-else>
+      <!-- <q-card-section v-else>
         <q-spinner size="lg" color="primary" />
-      </q-card-section>
+      </q-card-section> -->
     </q-card>
   </q-page>
 </template>
@@ -39,10 +39,17 @@ import { usePatientStore } from "@/stores/pacient";
 
 const route = useRoute();
 const patient = ref(null);
+const patientId = route.params.id;
 
 const store = usePatientStore();
 
 onMounted(async () => {
-  store.fetchPatientDetails()
+  store.fetchPatientDetails(patientId);
 });
+
+const name = computed(() => store.selectedPatient?.name || "");
+const birth_date = computed(() => store.selectedPatient?.birth_date || "");
+const contact_info = computed(() => store.selectedPatient?.contact_info || "");
+const created_at = computed(() => store.selectedPatient?.created_at || "");
+const gender = computed(() => store.selectedPatient?.gender || "");
 </script>
